375725582f69c7a0344f86601c9950bed34a4a69
KHALID IDRISSI <150524142+khalididrissi@users.noreply.github.com>
2024-06-03 23:00:30 +0100

    Update AdsPro.js

--- AdsPro.js
+++ AdsPro.js
@@ -15,3 +15,30 @@ function blockElementsByClassPrefix(classPrefix) {
 // Call the function with the desired class prefixes
 blockElementsByClassPrefix('container');
 blockElementsByClassPrefix('div-gpt-ad-');
+blockElementsByClassPrefix('pl-6f4f');
+// 3isk Start 
+blockElementsByClassPrefix('logo');
+blockElementsByClassPrefix('header-items');
+blockElementsByClassPrefix('aplvideo');
+blockElementsByClassPrefix('main-container videoap-container');
+blockElementsByClassPrefix('sm_box_1694957651544');
+blockElementsByClassPrefix('header-wrapper');
+blockElementsByClassPrefix('footer');
+blockElementsByClassPrefix('row width_fix_300 my-20');
+blockElementsByClassPrefix('row width_fix_300 watch-row-2 my-20');
+blockElementsByClassPrefix('my-40');
+blockElementsByClassPrefix('div-gpt-ad-1698946296176-0');
+blockElementsByClassPrefix('div-gpt-ad-1698946361303-0');
+blockElementsByClassPrefix('div-gpt-ad-8176806-1');
+blockElementsByClassPrefix('blog-post');
+blockElementsByClassPrefix('comments-container');
+blockElementsByClassPrefix('abs-head');
+blockElementsByClassPrefix('modal fade');
+blockElementsByClassPrefix('wrapper justify-content-between align-items-center');
+blockElementsByClassPrefix('abs');
+blockElementsByClassPrefix('end');
+blockElementsByClassPrefix('aside-wrapper mt-5');
+blockElementsByClassPrefix('mb-3 text-center');
+blockElementsByClassPrefix('fade modal');
+blockElementsByClassPrefix('d-flex justify-content-center my-3');
+//END 3ISK
