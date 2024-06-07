f10f363375e0e08a0726af7c2b04dd198727217c
KHALID IDRISSI <150524142+khalididrissi@users.noreply.github.com>
2024-06-07 23:30:41 +0100

    all

--- AdsPro.js
+++ AdsPro.js
@@ -1,26 +1,54 @@
- function blockElementsByClass(className) {
-            var elements = document.getElementsByClassName(className);
+// Block elements by class name
+function blockElementsByClass(className) {
+    var elements = document.getElementsByClassName(className);
 
-            // Remove elements from the DOM
-            while (elements.length > 0) {
-                elements[0].parentNode.removeChild(elements[0]);
-            }
-        }
+    // Remove elements from the DOM
+    while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+// Block elements by selector
+function blockElementsBySelector(selector) {
+    var elements = document.querySelectorAll(selector);
+
+    // Remove elements from the DOM
+    elements.forEach(function(element) {
+        element.parentNode.removeChild(element);
+    });
+}
+
+// Block the popup container
+function blockPopupContainer() {
+    var popupContainers = document.querySelectorAll('iframe[class*="container-6f4f5c3f5bfa5f5651799c658cb3556b"][style*="position: fixed;"]');
+    popupContainers.forEach(function(container) {
+        container.parentNode.removeChild(container);
+    });
+}
+
+// Block common ad selectors
+blockElementsBySelector('iframe[src*=".doubleclick.net"]');
+blockElementsBySelector('iframe[src*=".googleadservices.com"]');
+blockElementsBySelector('div[id*="google_ads_iframe"]');
+blockElementsBySelector('div[id*="div-gpt-ad"]');
+blockElementsBySelector('ins.adsbygoogle');
+blockElementsBySelector('div.ad-unit');
+blockElementsBySelector('div.ad-container');
+blockElementsBySelector('div.ad-banner');
+blockElementsBySelector('div.interstitial-ad');
+blockElementsBySelector('div.banner-ad');
+
+// Block additional ad selectors (add more as needed)
+blockElementsBySelector('div[id*="ad"]');
+blockElementsBySelector('div[class*="ad"]');
+blockElementsBySelector('div[id*="banner"]');
+blockElementsBySelector('div[class*="banner"]');
 
 
+// Block the popup container
+blockPopupContainer();
 
-
-blockElementsByClass('div-gpt-ad-');
-blockElementsByClass('pl-6f4f');
-blockElementsByClass('PageHeader');
-blockElementsByClass('SlideMenu');
-blockElementsByClass('relatedPosts');
-blockElementsByClass('row');
-blockElementsByClass('AboElSeed');
-blockElementsByClass('ads-');
-blockElementsByClass('banner-container');
-
-// 3isk Start 
+// 3isk Start
 blockElementsByClass('logo');
 blockElementsByClass('header-items');
 blockElementsByClass('aplvideo');
@@ -45,54 +73,75 @@ blockElementsByClass('aside-wrapper mt-5');
 blockElementsByClass('mb-3 text-center');
 blockElementsByClass('fade modal');
 blockElementsByClass('d-flex justify-content-center my-3');
-//END 3ISK
-blockElementsByClass('banner');
-blockElementsByClass('fixedban');
-blockElementsByClass('ps_ad');
-blockElementsByClass('live-ad');
+// 3isk End
+
+//ArabSeed Start
+blockElementsByClass('PageHeader');
+blockElementsByClass('SlideMenu');
+blockElementsByClass('relatedPosts');
+blockElementsByClass('row');
+blockElementsByClass('AboElSeed');
+blockElementsByClass('ads-aa');
+blockElementsByClass('pl-6f4f5c3f5bfa5f5651799c658cb3556b__wrap');
+blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b40743');
+blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b6839');
+blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b49835');
+blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b13062');
+blockElementsByClass('GoogleCreativeContainerClass');
+blockElementsByClass('pl-6f4f5c3f5bfa5f5651799c658cb3556b__wrap');
+//End Page 1
+blockElementsByClass('MainMenuOverlay');
+blockElementsByClass('mys-wrapper');
+blockElementsByClass('GoogleActiveViewInnerContainer');
+blockElementsByClass('div-over');
+blockElementsByClass('WatcherArea');
+blockElementsByClass('videoAdUi');
 blockElementsByClass('sarhne--banr');
-blockElementsByClass('live-ad-container');
+blockElementsByClass('ima-sdk-frame');
+blockElementsByClass('HeaderLogo');
+blockElementsByClass('downloadBTn');
+blockElementsByClass('div-over');
+blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b32535');
+blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b62840');
+blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b9969');
+blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b8124');
+blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b12185');
+blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b41932');
+blockElementsByClass('container-6f4f5c3f5bfa5f5651799c658cb3556b71401');
 
+// Remove elements by ID
+var elementsToRemoveById = [
+    'div-gpt-ad-1714577946689-0',
+    'div-gpt-ad-1715073293748-0',
+    'ps_ad_rotation_id_5446',
+    'ad_unit',
+    'fixedban2',
+    'fixedban',
+    'fixedban5',
+    'gpt_unit_/21727820151,22953604694/Gametech_HM/video-ad2_0',
+    'container-6f4f5c3f5bfa5f5651799c658cb3556b44923',
+    'div-gpt-ad-1714577932847-0',
+    'google_ads_iframe_/21727820151,22953604694/Gametech_HM/Interstitial-gameland_0__container__',
+    'gpt_unit_/21727820151,22953604694/Gametech_HM/Interstitial-gameland_0',
+    'id-custom_banner',
+    'google_ads_iframe_/22654869840/apl_1__container__',
+    'google_ads_iframe_/22893379435/apl_0__container__',
+    'dontfoid',
+    'gpt_unit_/7047,22643491855/apl/anchor/anchortop_0',
+    'banner-container',
+    'goog_2013926817',
+    'google_ads_iframe_/21727820151,22953604694/Gametech_HM/video-ad2_0__container__'
+];
 
+elementsToRemoveById.forEach(function(elementId) {
+    var element = document.getElementById(elementId);
+    if (element) {
+        element.remove();
+    }
+});
 
-
-
-
-
-
-
-        // Remove elements by ID
-        var elementsToRemoveById = [
-            'div-gpt-ad-1714577946689-0',
-            'div-gpt-ad-1715073293748-0',
-            'ps_ad_rotation_id_5446',
-            'ad_unit',
-            'fixedban2',
-            'fixedban',
-            'banner-container',
-            'ad_position_box',
-            'gpt_unit_/21727820151,22953604694/Gametech_HM/video-ad2_0',
-            'container-6f4f5c3f5bfa5f5651799c658cb3556b44923',
-            'div-gpt-ad-1714577932847-0',
-            'google_ads_iframe_/21727820151,22953604694/Gametech_HM/Interstitial-gameland_0__container__',
-            'gpt_unit_/21727820151,22953604694/Gametech_HM/Interstitial-gameland_0',
-            'ps_ad_rotation_id_5200',
-            'banner1',
-            'banner2',
-            'fixedban5',
-
-
-        ];
-
-        elementsToRemoveById.forEach(function(elementId) {
-            var element = document.getElementById(elementId);
-            if (element) {
-                element.remove();
-            }
-        });
-
-        // Remove additional aplvideo elements by class name
-        var aplvideoDivs = document.getElementsByClassName('aplvideo');
-        while (aplvideoDivs.length > 0) {
-            aplvideoDivs[0].remove();
-        }
+// Remove additional aplvideo elements by class name
+var aplvideoDivs = document.getElementsByClassName('aplvideo');
+while (aplvideoDivs.length > 0) {
+    aplvideoDivs[0].remove();
+}
