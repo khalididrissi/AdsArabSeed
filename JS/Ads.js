12412cf0085eb04348edcfa38ac8abd61bf4eb56
KHALID IDRISSI <150524142+khalididrissi@users.noreply.github.com>
2024-06-20 18:54:47 +0100

    a

--- JS/Ads.js
+++ JS/Ads.js
@@ -48,6 +48,7 @@ blockElementsByClass('card my-4');
 
 blockElementsByClass('py-1 bg-dark');
 blockElementsByClass('card-footer text-muted fs-6 px-1');
+blockElementsByClass('card');
 
 // 3isk End
 
